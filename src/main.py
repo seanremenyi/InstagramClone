3d49ff31286096051d11d44a9ee6e4842828c947
Ubuntu <ubuntu@ip-172-31-42-17.ec2.internal>
2020-11-11 07:07:36 +0000

    flask server running

--- src/main.py
+++ src/main.py
@@ -1,5 +1,62 @@
+from flask import Flask, jsonify, request
+app= Flask(__name__)
+import psycopg2
 
-def add(a,b):
-    return (a+b)
+connection = psycopg2.connect(
+    database="instagramclone",
+    user="ig",
+    password="instagram",
+    host="localhost",
+    port=5432
+    )
+    
+cursor = connection.cursor()
+
+@app.route("/<profileid>/pictures", methods=["GET"])
+def profile_pictures(profileid):
+    #Return all pictures
+    sql = f"SELECT * From pictures where username='{profileid}'"
+    cursor.execute(sql)
+    pictures = cursor.fetchall()
+    return jsonify(pictures)
+    
+@app.route("/<profileid>/<pictureid>", methods=["GET"])
+def picture_index(profileid,pictureid):
+    #Return all pictures
+    sql = f"SELECT * From pictures where pictureID={pictureid}"
+    cursor.execute(sql)
+    pictures = cursor.fetchall()
+    return jsonify(pictures)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def add(a,b):
+#     return (a+b)
     
 
